7f0c0369a0c6cdb40df6d8d8b9625f4a7f91e7a1
jacob <jacob@2simple.com>
2022-08-16 12:12:28 +0100

    Fixed defaultFontSize configuration option

--- src/core.js
+++ src/core.js
@@ -77,6 +77,7 @@ luckysheet.create = function (setting) {
     Store.limitSheetNameLength =  extendsetting.limitSheetNameLength;
     Store.defaultSheetNameMaxLength = extendsetting.defaultSheetNameMaxLength;
     Store.fontList = extendsetting.fontList;
+    Store.defaultFontSize = extendsetting.defaultFontSize;
     server.gridKey = extendsetting.gridKey;
     server.loadUrl = extendsetting.loadUrl;
     server.updateUrl = extendsetting.updateUrl;
